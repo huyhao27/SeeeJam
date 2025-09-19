41f48d8dcf49005718b1e2eb4e048c42f51199de
hvu2005 <hoangvu241205@gmail.com>
2025-09-19 20:21:08 +0700

    update xp system

---
+++ Assets/_Scripts/UI/LevelUpPopup.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelUpPopup : Popup
+{
+    [SerializeField] private List<GameObject> levelUpPanels;
+
+    private void RollPanel(GameObject panel)
+    {
+        
+    }
+}
